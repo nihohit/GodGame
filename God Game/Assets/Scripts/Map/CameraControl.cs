66f2d60848321239829e376483af7bfad0799eaf
Shachar <nihohit@gmail.com>
2017-08-27 07:52:42 +0300

    Camera movement (without rotation)

---
+++ God Game/Assets/Scripts/Map/CameraControl.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    // Zooming
+    public float dampTime = 0.2f;                 // Approximate time for the camera to refocus.
+    public float minZoom = 20f; // The smallest orthographic size the camera can be.
+    public float maxZoom = 90f;
+    public float zoomcChangePerFrame = 25f;
+    
+    // Moving
+    public float moveSpeed = 20f;
+
+    // Rotating
+    public float rotationSpeed = 10f;
+
+    // Pitch
+    public float pitchSpeed = 10f;
+
+    private Camera cameraObject;                        // Used for referencing the camera.
+    private float zoomSpeed;                      // Reference speed for the smooth damping of the orthographic size.
+    private Vector3 moveVelocity;                 // Reference velocity for the smooth damping of the position.
+    private Vector3 desiredPosition;              // The position the camera is moving towards.
+
+
+    private void Awake() {
+        cameraObject = GetComponentInChildren<Camera>();
+    }
+
+
+    private void FixedUpdate() {
+        // Move the camera towards a desired position.
+        move();
+
+        // Change the size of the camera based.
+        zoom();
+
+        rotate();
+
+        pitch();
+    }
+
+
+    private void move() {
+        var movementDirection = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) {
+            movementDirection += Vector3.right;
+            movementDirection += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            movementDirection += Vector3.forward;
+            movementDirection += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            movementDirection += Vector3.left;
+            movementDirection += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            movementDirection += Vector3.back;
+            movementDirection += Vector3.right;
+        }
+        movementDirection = movementDirection * moveSpeed * Time.deltaTime;
+        transform.parent.position += movementDirection;
+    }
+
+
+    private void zoom() {
+        float zoomChange = Input.mouseScrollDelta.y;
+        
+        // Find the required size based on the desired position and smoothly transition to that size.
+        float requiredSize = cameraObject.orthographicSize - zoomChange * zoomcChangePerFrame;
+        cameraObject.orthographicSize = Mathf.SmoothDamp(cameraObject.orthographicSize, requiredSize, ref zoomSpeed, dampTime);
+        cameraObject.orthographicSize = Mathf.Clamp(cameraObject.orthographicSize, minZoom, maxZoom);
+    }
+
+    void rotate() {
+
+    }
+
+    void pitch() {
+        if (!Input.GetMouseButton(2)) {
+            return;
+        }
+
+        float pitchChange = Input.GetAxis("Mouse Y") * pitchSpeed;
+        transform.parent.Rotate(Vector3.left * pitchChange * Time.deltaTime);
+    }
+}
--- God Game/Assets/Scripts/Map/TileScript.cs
+++ God Game/Assets/Scripts/Map/TileScript.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 
 public class TileScript : MonoBehaviour {
 
+    private int heightChange;
+
     public IEnumerable<TileScript> directNeighbours { get; set; }
 
     public IEnumerable<TileScript> indirectNeighbours { get; set; }
+
+    public void updateVertices(float heightChange, Vector3 offset) {
+        Debug.Log(transform.name);
+        List<Vector3> myVertices = new List<Vector3>();
+        List<Vector3> newVertices = new List<Vector3>();
+        var filter = transform.GetComponent<MeshFilter>();
+        var mesh = filter.mesh;
+        mesh.GetVertices(myVertices);
+        foreach (var vertex in myVertices) {
+            var offsetedVertex = vertex + offset;
+            int index = myVertices.IndexOf(offsetedVertex);
+            if (index >= 0) {
+                newVertices.Add(vertex + new Vector3(0, heightChange, 0));
+            } else {
+                newVertices.Add(vertex);
+            }
+        }
+        mesh.SetVertices(newVertices);
+
+        Debug.Log(string.Join(", ", mesh.vertices));
+        Debug.Log(string.Join(", ", GetComponent<MeshCollider>().sharedMesh.vertices));
+    }
 
     // Use this for initialization
     void Start () {
@@ -15,6 +39,29 @@ public class TileScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (heightChange != 0) {
+            float change = heightChange * Time.deltaTime;
+            updateVertices(heightChange, Vector3.zero);
+            foreach (var tile in directNeighbours) {
+                tile.updateVertices(heightChange, tile.transform.position - transform.position);
+            }
+            foreach(var tile in indirectNeighbours) {
+                tile.updateVertices(heightChange, tile.transform.position - transform.position);
+            }
+            heightChange = 0;
+        }
 	}
+
+    private void OnMouseOver() {
+        if (Input.GetMouseButton(0)) {
+            heightChange = 1;
+        }
+        if (Input.GetMouseButton(1)) {
+            heightChange = -1;
+        }
+    }
+
+    private void OnMouseExit() {
+        heightChange = 0;
+    }
 }
