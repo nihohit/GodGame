96f02391a145033453c939d7dd231833a37e8ade
Shachar Lang <shachar@lightricks.com>
2017-08-28 23:32:41 +0300

    TileScript: Better updating.

---
+++ God Game/Assets/Editor/TileScriptTests.cs
@@ -0,0 +1,196 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+public class TileScriptTests {
+
+	[Test]
+	public void adjustCornersTest() {
+		var gameObject = new GameObject();
+		var tile = gameObject.AddComponent<TileScript>();
+
+		var corners = new List<Vector3>{
+			new Vector3(-5, 1, -5), 
+			new Vector3(5, 2, -5), 
+			new Vector3(-5, 3, 5), 
+			new Vector3(5, 4, 5)
+		};
+		var heightChange = 5;
+
+		var offset = new Vector3(10, 0, 0);
+		var newCorners = tile.adjustCorners(heightChange, offset, corners);
+		var expectedCorners = new List<Vector3> {
+			new Vector3(-5, 1, -5), 
+			new Vector3(5, 7, -5), 
+			new Vector3(-5, 3, 5), 
+			new Vector3(5, 9, 5)
+		};
+		CollectionAssert.AreEqual(newCorners, expectedCorners);
+		
+		offset = new Vector3(0, 0, 10);
+		newCorners = tile.adjustCorners(heightChange, offset, corners);
+		expectedCorners = new List<Vector3> {
+			new Vector3(-5, 1, -5), 
+			new Vector3(5, 2, -5), 
+			new Vector3(-5, 8, 5), 
+			new Vector3(5, 9, 5)
+		};
+		CollectionAssert.AreEqual(newCorners, expectedCorners);
+
+		offset = new Vector3(-10, 0, 0);
+		newCorners = tile.adjustCorners(heightChange, offset, corners);
+		expectedCorners = new List<Vector3> {
+			new Vector3(-5, 6, -5), 
+			new Vector3(5, 2, -5), 
+			new Vector3(-5, 8, 5), 
+			new Vector3(5, 4, 5)
+		};
+		CollectionAssert.AreEqual(newCorners, expectedCorners);
+		
+		offset = new Vector3(0, 0, -10);
+		newCorners = tile.adjustCorners(heightChange, offset, corners);
+		expectedCorners = new List<Vector3> {
+			new Vector3(-5, 6, -5), 
+			new Vector3(5, 7, -5), 
+			new Vector3(-5, 3, 5), 
+			new Vector3(5, 4, 5)
+		};
+		CollectionAssert.AreEqual(newCorners, expectedCorners);
+	}
+
+	[Test]
+	public void adjustIndirectCornersTest() {
+		var gameObject = new GameObject();
+		var tile = gameObject.AddComponent<TileScript>();
+
+		var corners = new List<Vector3>{
+			new Vector3(-5, 1, -5), 
+			new Vector3(5, 2, -5), 
+			new Vector3(-5, 3, 5), 
+			new Vector3(5, 4, 5)
+		};
+		var heightChange = 5;
+
+		var offset = new Vector3(10, 0, 10);
+		var newCorners = tile.adjustCorners(heightChange, offset, corners);
+		var expectedCorners = new List<Vector3> {
+			new Vector3(-5, 1, -5), 
+			new Vector3(5, 2, -5), 
+			new Vector3(-5, 3, 5), 
+			new Vector3(5, 9, 5)
+		};
+		CollectionAssert.AreEqual(newCorners, expectedCorners);
+		
+		offset = new Vector3(-10, 0, 10);
+		newCorners = tile.adjustCorners(heightChange, offset, corners);
+		expectedCorners = new List<Vector3> {
+			new Vector3(-5, 1, -5), 
+			new Vector3(5, 2, -5), 
+			new Vector3(-5, 8, 5), 
+			new Vector3(5, 4, 5)
+		};
+		CollectionAssert.AreEqual(newCorners, expectedCorners);
+
+		offset = new Vector3(-10, 0, -10);
+		newCorners = tile.adjustCorners(heightChange, offset, corners);
+		expectedCorners = new List<Vector3> {
+			new Vector3(-5, 6, -5), 
+			new Vector3(5, 2, -5), 
+			new Vector3(-5, 3, 5), 
+			new Vector3(5, 4, 5)
+		};
+		CollectionAssert.AreEqual(newCorners, expectedCorners);
+		
+		offset = new Vector3(10, 0, -10);
+		newCorners = tile.adjustCorners(heightChange, offset, corners);
+		expectedCorners = new List<Vector3> {
+			new Vector3(-5, 1, -5), 
+			new Vector3(5, 7, -5), 
+			new Vector3(-5, 3, 5), 
+			new Vector3(5, 4, 5)
+		};
+		CollectionAssert.AreEqual(newCorners, expectedCorners);
+	}
+
+	[Test]
+	public void adjustVertices() {
+		var gameObject = new GameObject();
+		var tile = gameObject.AddComponent<TileScript>();
+
+		var vertices = new List<Vector3>{
+			new Vector3(-1, 0, -1),
+			new Vector3(0, 0, -1), 
+			new Vector3(1, 0, -1), 
+			new Vector3(-1, 0, 0),
+			new Vector3(0, 0, 0), 
+			new Vector3(1, 0, 0), 
+			new Vector3(-1, 0, 1),
+			new Vector3(0, 0, 1), 
+			new Vector3(1, 0, 1), 
+		}; 
+
+		var corners = new List<Vector3>{
+			new Vector3(-1, 2, -1), 
+			new Vector3(1, 2, -1), 
+			new Vector3(-1, 0, 1), 
+			new Vector3(1, 0, 1)
+		};
+		var expectedVertices = new List<Vector3>{
+			new Vector3(-1, 2, -1),
+			new Vector3(0, 2, -1), 
+			new Vector3(1, 2, -1), 
+			new Vector3(-1, 1, 0),
+			new Vector3(0, 1, 0), 
+			new Vector3(1, 1, 0), 
+			new Vector3(-1, 0, 1),
+			new Vector3(0, 0, 1), 
+			new Vector3(1, 0, 1), 
+		};
+
+		var result = tile.adjustVertices(vertices, corners);
+
+		CollectionAssert.AreEqual(result, expectedVertices); 
+	}
+
+		[Test]
+	public void adjustVerticesSingleCorner() {
+		var gameObject = new GameObject();
+		var tile = gameObject.AddComponent<TileScript>();
+
+		var vertices = new List<Vector3>{
+			new Vector3(-1, 0, -1),
+			new Vector3(0, 0, -1), 
+			new Vector3(1, 0, -1), 
+			new Vector3(-1, 0, 0),
+			new Vector3(0, 0, 0), 
+			new Vector3(1, 0, 0), 
+			new Vector3(-1, 0, 1),
+			new Vector3(0, 0, 1), 
+			new Vector3(1, 0, 1), 
+		}; 
+
+		var corners = new List<Vector3>{
+			new Vector3(-1, 4, -1), 
+			new Vector3(1, 0, -1), 
+			new Vector3(-1, 0, 1), 
+			new Vector3(1, 0, 1)
+		};
+		var expectedVertices = new List<Vector3>{
+			new Vector3(-1, 4, -1),
+			new Vector3(0, 2, -1), 
+			new Vector3(1, 2, -1), 
+			new Vector3(-1, 1, 0),
+			new Vector3(0, 1, 0), 
+			new Vector3(1, 0, 0), 
+			new Vector3(-1, 0, 1),
+			new Vector3(0, 0, 1), 
+			new Vector3(1, 0, 1), 
+		};
+
+		var result = tile.adjustVertices(vertices, corners);
+
+		CollectionAssert.AreEqual(result, expectedVertices); 
+	}
+}
--- God Game/Assets/Scripts/Map/TileScript.cs
+++ God Game/Assets/Scripts/Map/TileScript.cs
@@ -1,52 +1,101 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TileScript : MonoBehaviour {
 
     private int heightChange;
 
+    private int sqrtOfMesh;
+
     public IEnumerable<TileScript> directNeighbours { get; set; }
 
     public IEnumerable<TileScript> indirectNeighbours { get; set; }
 
     public void updateVertices(float heightChange, Vector3 offset) {
-        Debug.Log(transform.name);
-        List<Vector3> myVertices = new List<Vector3>();
-        List<Vector3> newVertices = new List<Vector3>();
-        var filter = transform.GetComponent<MeshFilter>();
-        var mesh = filter.mesh;
-        mesh.GetVertices(myVertices);
-        foreach (var vertex in myVertices) {
-            var offsetedVertex = vertex + offset;
-            int index = myVertices.IndexOf(offsetedVertex);
-            if (index >= 0) {
-                newVertices.Add(vertex + new Vector3(0, heightChange, 0));
-            } else {
-                newVertices.Add(vertex);
-            }
-        }
-        mesh.SetVertices(newVertices);
+        var vertices = new List<Vector3>();
+        var mesh = transform.GetComponent<MeshFilter>().mesh;
+        mesh.GetVertices(vertices);
+        var corners = getCorners(vertices);
+        var newCorners = adjustCorners(heightChange, offset, corners);
+        var newVertices = adjustVertices(vertices, newCorners);
+        mesh.SetVertices(newVertices.ToList());
+    }
 
-        Debug.Log(string.Join(", ", mesh.vertices));
-        Debug.Log(string.Join(", ", GetComponent<MeshCollider>().sharedMesh.vertices));
+    public IEnumerable<Vector3> adjustCorners(float heightChange, Vector3 offset, IEnumerable<Vector3> corners) {
+        return corners.Select(corner => {
+            bool xCompatible = offset.x == 0 || Math.Sign(offset.x) == Math.Sign(corner.x);
+            bool yCompatible = offset.z == 0 || Math.Sign(offset.z) == Math.Sign(corner.z);
+            if (xCompatible && yCompatible) {
+                var result = corner + new Vector3(0, heightChange, 0);
+                return result;
+            }
+            return corner;
+        });
+    }
+
+    public IEnumerable<Vector3> adjustVertices(IEnumerable<Vector3> vertices, IEnumerable<Vector3> adjustedCorners) {
+        var center = adjustedCorners.Aggregate((sum, corner) => sum + corner) / 4;
+        return vertices.Select(vertex => {
+            var distances = adjustedCorners.OrderBy(corner => twoDimensionalDistance(corner, vertex)).ToList();
+            Debug.Log(vertex + ": " + string.Join(", ", distances));
+            return bariocentricInterpolation(vertex, center, distances[0], distances[1]);
+        });
+    }
+
+    private Vector3 bariocentricInterpolation(Vector3 point, Vector3 p1, Vector3 p2, Vector3 p3) {
+        var f1 = p1-point;
+        f1.y = 0;
+        var f2 = p2-point;
+        f2.y = 0;
+        var f3 = p3-point;
+        f3.y = 0;
+        // calculate the areas and factors (order of parameters doesn't matter):
+        var a = Vector3.Cross(p1-p2, p1-p3).magnitude; // main triangle area a
+        var a1 = Vector3.Cross(f2, f3).magnitude / a; // p1's triangle area / a
+        var a2 = Vector3.Cross(f3, f1).magnitude / a; // p2's triangle area / a 
+        var a3 = Vector3.Cross(f1, f2).magnitude / a; // p3's triangle area / a
+        // find the uv corresponding to point f (uv1/uv2/uv3 are associated to p1/p2/p3):
+        point.y = a1 * p1.y + a2 * p2.y + a3 * p3.y;
+        return point;
+    } 
+
+    private float twoDimensionalDistance(Vector3 first, Vector3 second) {
+        first.y = 0;
+        second.y = 0;
+        return Vector3.Distance(first, second);
     }
 
     // Use this for initialization
     void Start () {
-        GetComponent<MeshCollider>().sharedMesh = transform.GetComponent<MeshFilter>().mesh;
+        var mesh = transform.GetComponent<MeshFilter>().mesh;
+        GetComponent<MeshCollider>().sharedMesh = mesh;
+        List<Vector3> myVertices = new List<Vector3>();
+        mesh.GetVertices(myVertices);
+        sqrtOfMesh = Convert.ToInt32(Math.Sqrt(myVertices.Count));
+    }
+
+    private List<Vector3> getCorners(List<Vector3> vertices) {
+        return new List<Vector3> { 
+            vertices[0], 
+            vertices[sqrtOfMesh - 1], 
+            vertices[vertices.Count - sqrtOfMesh], 
+            vertices[vertices.Count - 1]
+        };
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (heightChange != 0) {
             float change = heightChange * Time.deltaTime;
-            updateVertices(heightChange, Vector3.zero);
+            updateVertices(change, Vector3.zero);
             foreach (var tile in directNeighbours) {
-                tile.updateVertices(heightChange, tile.transform.position - transform.position);
+                tile.updateVertices(change, tile.transform.position - transform.position);
             }
             foreach(var tile in indirectNeighbours) {
-                tile.updateVertices(heightChange, tile.transform.position - transform.position);
+                tile.updateVertices(change, tile.transform.position - transform.position);
             }
             heightChange = 0;
         }
@@ -54,10 +103,10 @@ public class TileScript : MonoBehaviour {
 
     private void OnMouseOver() {
         if (Input.GetMouseButton(0)) {
-            heightChange = 1;
+            heightChange = 10;
         }
         if (Input.GetMouseButton(1)) {
-            heightChange = -1;
+            heightChange = -10;
         }
     }
 
