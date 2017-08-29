e29e09f2ea1168cbf819a157d35d98092c909743
Shachar Lang <shachar@lightricks.com>
2017-08-29 07:56:53 +0300

    Terrain transformation works.

--- God Game/Assets/Scripts/Map/TileScript.cs
+++ God Game/Assets/Scripts/Map/TileScript.cs
@@ -18,71 +18,59 @@ public class TileScript : MonoBehaviour {
         var vertices = new List<Vector3>();
         var mesh = transform.GetComponent<MeshFilter>().mesh;
         mesh.GetVertices(vertices);
-        var corners = getCorners(vertices);
-        var newCorners = adjustCorners(heightChange, offset, corners);
-        var newVertices = adjustVertices(vertices, newCorners);
+        var newVertices = adjustVertices(vertices, heightChange, offset);
         mesh.SetVertices(newVertices.ToList());
+        GetComponent<MeshCollider>().sharedMesh = mesh;
     }
 
-    public IEnumerable<Vector3> adjustCorners(float heightChange, Vector3 offset, IEnumerable<Vector3> corners) {
-        return corners.Select(corner => {
-            bool xCompatible = offset.x == 0 || Math.Sign(offset.x) == Math.Sign(corner.x);
-            bool yCompatible = offset.z == 0 || Math.Sign(offset.z) == Math.Sign(corner.z);
+    public IEnumerable<Vector3> adjustVertices(IEnumerable<Vector3> vertices, float heightChange, Vector3 offset) {
+        var adjustedVertices = vertices.Select(vertex => {
+            bool xCompatible = offset.x == 0 || Math.Sign(offset.x) == Math.Sign(vertex.x);
+            bool yCompatible = offset.z == 0 || Math.Sign(offset.z) == Math.Sign(vertex.z);
             if (xCompatible && yCompatible) {
-                var result = corner + new Vector3(0, heightChange, 0);
+                var result = vertex + new Vector3(0, heightChange, 0);
                 return result;
             }
-            return corner;
-        });
-    }
+            return vertex;
+        }).ToList();
 
-    public IEnumerable<Vector3> adjustVertices(IEnumerable<Vector3> vertices, IEnumerable<Vector3> adjustedCorners) {
-        var center = adjustedCorners.Aggregate((sum, corner) => sum + corner) / 4;
-        return vertices.Select(vertex => {
-            var distances = adjustedCorners.OrderBy(corner => twoDimensionalDistance(corner, vertex)).ToList();
-            Debug.Log(vertex + ": " + string.Join(", ", distances));
-            return bariocentricInterpolation(vertex, center, distances[0], distances[1]);
-        });
-    }
-
-    private Vector3 bariocentricInterpolation(Vector3 point, Vector3 p1, Vector3 p2, Vector3 p3) {
-        var f1 = p1-point;
-        f1.y = 0;
-        var f2 = p2-point;
-        f2.y = 0;
-        var f3 = p3-point;
-        f3.y = 0;
-        // calculate the areas and factors (order of parameters doesn't matter):
-        var a = Vector3.Cross(p1-p2, p1-p3).magnitude; // main triangle area a
-        var a1 = Vector3.Cross(f2, f3).magnitude / a; // p1's triangle area / a
-        var a2 = Vector3.Cross(f3, f1).magnitude / a; // p2's triangle area / a 
-        var a3 = Vector3.Cross(f1, f2).magnitude / a; // p3's triangle area / a
-        // find the uv corresponding to point f (uv1/uv2/uv3 are associated to p1/p2/p3):
-        point.y = a1 * p1.y + a2 * p2.y + a3 * p3.y;
-        return point;
-    } 
-
-    private float twoDimensionalDistance(Vector3 first, Vector3 second) {
-        first.y = 0;
-        second.y = 0;
-        return Vector3.Distance(first, second);
+        adjustedVertices[2] = getCorners(adjustedVertices).Aggregate((sum, corner) => sum + corner) / 4;
+        return adjustedVertices;
     }
 
     // Use this for initialization
     void Start () {
         var mesh = transform.GetComponent<MeshFilter>().mesh;
         GetComponent<MeshCollider>().sharedMesh = mesh;
-        List<Vector3> myVertices = new List<Vector3>();
-        mesh.GetVertices(myVertices);
-        sqrtOfMesh = Convert.ToInt32(Math.Sqrt(myVertices.Count));
+        mesh.Clear();
+        mesh.SetVertices(new List<Vector3> {
+            new Vector3(-5, 0, -5),
+            new Vector3(5, 0, -5),
+            new Vector3(0, 0, 0),
+            new Vector3(-5, 0, 5),
+            new Vector3(5, 0, 5)
+        });
+        mesh.uv = new Vector2[] {
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(0.5f, 0.5f),
+            new Vector2(0, 1),
+            new Vector2(1, 1)
+        };
+        mesh.triangles = new int[] {
+            2, 1, 0,
+            3, 2, 0,
+            1, 2, 4,
+            2, 3, 4
+        };
     }
 
     private List<Vector3> getCorners(List<Vector3> vertices) {
         return new List<Vector3> { 
             vertices[0], 
-            vertices[sqrtOfMesh - 1], 
-            vertices[vertices.Count - sqrtOfMesh], 
-            vertices[vertices.Count - 1]
+            vertices[1], 
+            vertices[3], 
+            vertices[4]
         };
     }
 	
@@ -92,10 +80,10 @@ public class TileScript : MonoBehaviour {
             float change = heightChange * Time.deltaTime;
             updateVertices(change, Vector3.zero);
             foreach (var tile in directNeighbours) {
-                tile.updateVertices(change, tile.transform.position - transform.position);
+                tile.updateVertices(change, transform.position - tile.transform.position);
             }
             foreach(var tile in indirectNeighbours) {
-                tile.updateVertices(change, tile.transform.position - transform.position);
+                tile.updateVertices(change, transform.position - tile.transform.position);
             }
             heightChange = 0;
         }
