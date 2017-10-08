46ef59dbc072e154c9ae2e0c35c492c568d669e3
Shachar <nihohit@gmail.com>
2017-10-08 22:40:26 +0300

    FreeFallingObject: Initial commit.

---
+++ God Game/Assets/PlayModeTests/FreeFallingObjectPlayTests.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class FreeFallingObjectPlayTests {
+	[UnityTest]
+	public IEnumerator objectShouldBeDestroyedWhenTooHigh() {
+        var gameObject = new GameObject();
+        var fallingScripts = gameObject.AddComponent<FreeFallingObject>();
+
+		yield return null;
+
+        Assert.IsFalse(gameObject == null);
+
+        fallingScripts.transform.position = new Vector3(0, Constants.MaxHeight + 1, 0);
+
+        yield return null;
+        yield return null;
+
+        Assert.IsTrue(gameObject == null);
+    }
+
+    [UnityTest]
+    public IEnumerator objectShouldBeDestroyedWhenTooLow() {
+        var gameObject = new GameObject();
+        var fallingScripts = gameObject.AddComponent<FreeFallingObject>();
+
+        yield return null;
+
+        Assert.IsFalse(gameObject == null);
+
+        fallingScripts.transform.position = new Vector3(0, Constants.MaxHeight + 1, 0);
+
+        yield return null;
+        yield return null;
+
+        Assert.IsTrue(gameObject == null);
+    }
+}
---
+++ God Game/Assets/Scripts/Map/FreeFallingObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FreeFallingObject : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y > Constants.MaxHeight || transform.position.y < Constants.MinHeight) {
+            Destroy(gameObject);
+        }
+	}
+
+    public static void freeObject(Transform obj) {
+        obj.parent = null;
+        Rigidbody rigidBody = obj.gameObject.AddComponent<Rigidbody>();
+        rigidBody.mass = 5;
+        rigidBody.useGravity = true;
+        obj.GetComponent<Collider>().enabled = true;
+        obj.gameObject.AddComponent<FreeFallingObject>();
+    }
+}
