ab8f9aa2a0176815b77cb27e488fdcc7b9191a21
Shachar <nihohit@gmail.com>
2018-04-05 10:46:29 +0300

    temp

--- God Game/Assets/Scripts/Map/BoardScript.cs
+++ God Game/Assets/Scripts/Map/BoardScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using System;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer), typeof(MeshCollider))]
 public class BoardScript : MonoBehaviour {
@@ -36,14 +37,14 @@ public class BoardScript : MonoBehaviour {
     
     for (int i = 0; i <= x; i++) {
       for (int j = 0; j <= z; j++) {
-        var vertexIndex = i + (j * ((2 * x) + 1));
+        var vertexIndex = indexOfCornerVertex(i, j, x);
         vertices[vertexIndex] = new Vector3(i, 0, j) * Constants.SizeOfTile;
         uv[vertexIndex] = new Vector2(vertices[vertexIndex].x / x, vertices[vertexIndex].z / z) / Constants.SizeOfTile;
         if (i == x || j == z) {
           continue;
         }
 
-        var centerIndex = vertexIndex + x + 1;
+        var centerIndex = indexOfCenterVertex(i, j, x);
         vertices[centerIndex] = new Vector3(i + 0.5f, 0, j + 0.5f) * Constants.SizeOfTile;
         uv[centerIndex] = new Vector2(vertices[centerIndex].x / x, vertices[centerIndex].z / z) / Constants.SizeOfTile;
 
@@ -72,7 +73,15 @@ public class BoardScript : MonoBehaviour {
     return mesh;
   }
 
-  private GameObject instantiateObject(Object prefab, Vector3 position) {
+  static int indexOfCornerVertex(int xCoord, int zCoord, int maxX) {
+    return xCoord + (zCoord * ((2 * maxX) + 1));
+  }
+
+  static int indexOfCenterVertex(int xCoord, int zCoord, int maxX) {
+    return indexOfCornerVertex(xCoord, zCoord, maxX) + maxX + 1;
+  }
+
+  private GameObject instantiateObject(UnityEngine.Object prefab, Vector3 position) {
     return (GameObject)Instantiate(prefab, position, Quaternion.identity);
   }
 
@@ -99,8 +108,60 @@ public class BoardScript : MonoBehaviour {
       return;
     }
 
-    TileScript tileToUpdate = hit.collider.GetComponent<TileScript>();
-    updateTile(tileToUpdate, direction);
+    adjustVertices(hit.point, direction);
+  }
+
+  private void adjustVertices(Vector3 point, TileUpdateDirection direction) {
+    var resizedPoint = point / Constants.SizeOfTile;
+    var minX = Mathf.FloorToInt(resizedPoint.x);
+    var minZ = Mathf.FloorToInt(resizedPoint.z);
+
+    var vertices = filter.mesh.vertices;
+    adjustCornersUniformly(minX, minZ, vertices, direction);
+    adjustCenters(minX, minZ, vertices);
+
+    filter.mesh.SetVertices(vertices.ToList());
+    filter.mesh.RecalculateNormals();
+    meshCollider.sharedMesh = filter.mesh;
+  }
+
+  private void adjustCornersUniformly(int sourceX, int sourceZ, Vector3[] vertices, TileUpdateDirection direction) {
+    var change = new Vector3(0, heightChangeRate * Time.deltaTime, 0);
+    change = direction == TileUpdateDirection.Down ? -change : change;
+    var maxX = sourceX + 1;
+    var maxZ = sourceZ + 1;
+    vertices[indexOfCornerVertex(sourceX, sourceZ, x)] += change;
+    vertices[indexOfCornerVertex(maxX, sourceZ, x)] += change;
+    vertices[indexOfCornerVertex(sourceX, maxZ, x)] += change;
+    vertices[indexOfCornerVertex(maxX, maxZ, x)] += change;
+  }
+
+  private void adjustCenters(int sourceX, int sourceZ, Vector3[] vertices) {
+    var minX = sourceX - 1;
+    var minZ = sourceZ - 1;
+    var maxX = sourceX + 1;
+    var maxZ = sourceZ + 1;
+
+    adjustCenter(minX, minZ, vertices);
+    adjustCenter(minX, sourceZ, vertices);
+    adjustCenter(minX, maxZ, vertices);
+    adjustCenter(sourceX, minZ, vertices);
+    adjustCenter(sourceX, sourceZ, vertices);
+    adjustCenter(sourceX, maxZ, vertices);
+    adjustCenter(maxX, minZ, vertices);
+    adjustCenter(maxX, sourceZ, vertices);
+    adjustCenter(maxX, maxZ, vertices);
+  }
+
+  private void adjustCenter(int minX, int minZ, Vector3[] vertices) {
+    var maxX = minX + 1;
+    var maxZ = minZ + 1;
+    var accumulatedValues = Vector3.zero;
+    accumulatedValues += vertices[indexOfCornerVertex(minX, minZ, x)];
+    accumulatedValues += vertices[indexOfCornerVertex(maxX, minZ, x)];
+    accumulatedValues += vertices[indexOfCornerVertex(minX, maxZ, x)];
+    accumulatedValues += vertices[indexOfCornerVertex(maxX, maxZ, x)];
+    vertices[indexOfCenterVertex(minX, minZ, x)] = accumulatedValues / 4;
   }
 
   public void updateTile(TileScript tile, TileUpdateDirection direction) {
